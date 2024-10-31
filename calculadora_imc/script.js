d62f4b7dec6705d37622c2eb220629ca946fc295
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2024-10-31 20:40:39 -0300

    adicionado darkmode e whitemode

--- calculadora_imc/script.js
+++ calculadora_imc/script.js
@@ -39,3 +39,85 @@ function calcular() {
         document.getElementById("saidaRecomendado").value = String(pesoIdeal.toFixed(2));
     }   
 }
+
+function darkMode() {
+    //declarção das variaveis dos elementos do html
+    let corpo = document.getElementById("corpo");
+    let calculadora = document.getElementById("calculadora");
+    let cabeca_calcu = document.getElementById("cabeca");
+    let botao = document.getElementById("botao");
+    let altura = document.getElementById("altura");
+    let peso = document.getElementById("peso"); 
+    let saidaImc = document.getElementById("saidaImc");
+    let saidaClassificacao = document.getElementById("saidaClassificacao");
+    let saidaRecomendado = document.getElementById("saidaRecomendado");
+    let cabecaImc = document.getElementById("cabecaImc");
+    let cabecaClassificacao = document.getElementById("cabecaClassificacao");
+    let cabecarecomendado = document.getElementById("cabecaRecomendado");
+    let botao_darkMode = document.getElementById("botao_darkMode");
+    let cor = corpo.style.backgroundColor;
+
+    if(cor == "rgb(9, 9, 9)") {
+        //branco
+        corpo.style.backgroundColor = "rgb(247, 247, 247)";
+
+        calculadora.style.backgroundColor = "white";
+        calculadora.style.boxShadow = "3px 3px 5px rgb(199, 199, 199)";
+
+        cabeca_calcu.style.backgroundColor = "rgb(233, 233, 233)";
+        cabeca_calcu.style.color = "black";
+
+        botao.style.backgroundColor = "lightgrey";
+        botao.style.color = "black";
+
+        altura.style.backgroundColor = "white";
+        altura.style.boxShadow = "0px 2px rgb(233, 233, 233)";
+
+        peso.style.backgroundColor = "white"
+        peso.style.boxShadow = "0px 2px rgb(233, 233, 233)";
+
+        saidaImc.style.backgroundColor = "white";
+        saidaClassificacao.style.backgroundColor = "white";
+        saidaRecomendado.style.backgroundColor = "white";
+
+        cabecaImc.style.color = "black";
+        cabecaClassificacao.style.color = "black";
+        cabecarecomendado.style.color = "black";
+
+        botao_darkMode.style.backgroundColor = "rgb(233, 233, 233)",
+        botao_darkMode.style.color = "black";
+        botao_darkMode.innerText = "MODO ESCURO";
+
+    }else {
+        //preto
+        corpo.style.backgroundColor = "rgb(9, 9, 9)";
+
+        calculadora.style.backgroundColor = "rgb(20, 20, 20)";
+        calculadora.style.boxShadow = "3px 3px 5px rgb(20, 20, 20)";
+
+        cabeca_calcu.style.backgroundColor = "rgb(35, 35, 35)";
+        cabeca_calcu.style.color = "lightgray";
+
+        botao.style.backgroundColor = "rgb(40, 40, 40)";
+        botao.style.color = "rgb(200, 200, 200)";
+
+        altura.style.backgroundColor = "rgb(7, 7, 7)";
+        altura.style.boxShadow = "0px 2px rgb(35, 35, 35)";
+
+        peso.style.backgroundColor = "rgb(7, 7, 7)";
+        peso.style.boxShadow = "0px 2px rgb(35, 35, 35)";
+
+        saidaImc.style.backgroundColor = "rgb(20, 20, 20)";
+        saidaClassificacao.style.backgroundColor = "rgb(20, 20, 20)";
+        saidaRecomendado.style.backgroundColor = "rgb(20, 20, 20)";
+
+        cabecaImc.style.color = "lightgray";
+        cabecaClassificacao.style.color = "lightgray";
+        cabecarecomendado.style.color = "lightgray";
+
+        botao_darkMode.style.backgroundColor = "rgb(35, 35, 35)",
+        botao_darkMode.style.color = "lightgray";
+        botao_darkMode.innerText = "MODO CLARO";
+
+    }
+}
