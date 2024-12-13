ea12762d05e735ed0b40dc892ebbd379ffca4807
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2024-12-13 16:42:18 -0300

    projeto funcionando

--- atividades_genericas/4=10/script.js
+++ atividades_genericas/4=10/script.js
@@ -1,35 +1,106 @@
-let num1 = document.getElementById("num1").value;
-let num2 = document.getElementById("num2").value;
-let num3 = document.getElementById("num3").value;
-let num4 = document.getElementById("num4").value;
-let conta = "";
-let oper1 = "";
-let oper2 = "";
-let oper3 = "";
-let oper4 = "";
-let operCombi = []
 
-const operacoes = ["+","-","*","/"];
-const numeros = [num1,num2,num3,num4];
-const opers = []
 
-function combinacoes() {
-    for (let i = 1; i <= 4; i++) {
-        oper1 = i;
-        for (let i = 1; i <= 4; i++) {
-            oper2 = i;
-            for (let i = 1; i <= 4; i++) {
-                oper3 = i;
-                for (let i = 1; i <= 4; i++) {
-                    oper4 = i;
-                    operCombi = oper1 + oper2 + oper3 + oper4;
-                    return operCombi;
+function testaCombinacoes() {
+
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let num3 = document.getElementById("num3").value;
+    let num4 = document.getElementById("num4").value;
+    let posNum = "abcd";
+    let num = "";
+    let posOper = "abc";
+    let oper = "";
+    let posCont = "abcdefg"
+    let conta = ""
+
+    const operacoes = ["+","-","*","/"];
+    const numeros = [num1,num2,num3,num4];
+    const combinacoesNUM = [];
+    const combinacoesOPER = [];
+    const combinacoesCONTA = [];
+    const contaCerta = [];
+
+    function combinacoesNum() {
+        for (let a = 0; a < numeros.length; a++) {
+    
+            for (let b = 0; b < numeros.length; b++) {
+    
+                for (let c = 0; c < numeros.length; c++) {
+    
+                    for (let d = 0; d < numeros.length; d++) {
+    
+                        if (a != b && a != c && a != d) {
+                            if (b != c && b != d) {
+                                if (c != d) {
+                                    num = posNum.replace("a",numeros[a]).replace("b",numeros[b]).replace("c",numeros[c]).replace("d",numeros[d]);
+                                    combinacoesNUM.push(num);
+                                }
+                            }
+                        }
+    
+                    }
                 }
             }
         }
     }
+    
+    function reduz() {
+        for (let a = 0; a < combinacoesNUM.length; a++) {
+            for (let b = 0; b < combinacoesNUM.length; b++) {
+    
+                if (a != b) {
+                    if (combinacoesNUM[a] == combinacoesNUM[b]) {
+                        combinacoesNUM.splice(b,1);
+                    } 
+                }
+    
+            }    
+        }
+    }
+    
+    function combinacoesOper() {
+        for (let a = 0; a < operacoes.length; a++) {
+    
+            for (let b = 0; b < operacoes.length; b++) {
+    
+                for (let c = 0; c < operacoes.length; c++) {    
+    
+                    oper = posOper.replace("a",operacoes[a]).replace("b",operacoes[b]).replace("c",operacoes[c]);
+                    combinacoesOPER.push(oper);
+    
+                }
+            }
+        }
+    }
+    
+    function combinacoesConta() {
+        for (let a = 0; a < combinacoesNUM.length; a++) {
+    
+            for (let b = 0; b < combinacoesOPER.length; b++) {
+    
+                conta = posCont.replace("a",combinacoesNUM[a][0]).replace("b",combinacoesOPER[b][0]).replace("c",combinacoesNUM[a][1]).replace("d",combinacoesOPER[b][1]).replace("e",combinacoesNUM[a][2]).replace("f",combinacoesOPER[b][2]).replace("g",combinacoesNUM[a][3])
+                combinacoesCONTA.push(conta);
+    
+            }
+        }
+    }
+    
+    function validacao() {
+        for (let i = 0; i < combinacoesCONTA.length; i++) {
+            if (eval(combinacoesCONTA[i]) == 10) {
+                contaCerta.push(combinacoesCONTA[i]);
+            }
+            
+        }
+    }
+    
+    combinacoesNum();
+    reduz();
+    combinacoesOper();
+    combinacoesConta();
+    validacao();
+
+    console.log(contaCerta);
 }
 
-console.log(combinacoes());
-console.log(combinacoes());
 
